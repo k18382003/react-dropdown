1fbf94ec6922e02159ec486030236b076269376a
鄧莉瀕 <k18382003@gmail.com>
2024-04-06 12:28:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# React Modal
+
+<p>The dropdown select list is frequently utilized across different projects. While we can easily implement a dropdown menu feature using <select>, it may not offer the best user experience. The summer-ui-react-dropdown, on the other hand, enhances usability by providing support for multiple select features and incorporating a search bar, allowing users to filter the list before making a selection. This modal component is customizable, enabling the creation of personalized placeholder messages and font styling to suit individual preferences.</p>
+
+## Installation
+
+npm i summer-ui-react-dropdown
+
+## Demo
+
+https://k18382003.github.io/react-modal-dropdown/
+
+## Example
+
+```jsx
+// App.js
+import ReactDropdown from 'summer-ui-react-dropdown';
+import { useState } from 'react';
+import './App.css';
+import './react-dropdown.css';
+
+const App = () => {
+  const options = [
+    { displayName: 'Apple', value: 1 },
+    { displayName: 'Orange', value: 2 },
+    { displayName: 'Banana', value: 3 },
+  ];
+  const [selectedValues, setSelectedValues] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here you can perform any action with the selected data, such as sending it to a server
+    console.log({ ...selectedValues });
+
+    // Clearing the form after submission
+    setSelectedValues('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <CustomDropDown
+        options={options} //The select options must be [{displayName:'name', value:'value'}] format
+        multipleSelect={true} //A multiple choices list
+        selectedValues={selectedValues} //Selected values
+        setSelectedValues={setSelectedValues} //A UseState to set selectedValues variable
+        textbarPlaceHolder={'Favorite fruits'} //The placeholder text for the select input
+        textbarStyle={'mytextBar'} //Adding a class name to style select input
+        searchbarStyle={'mysearchBar'} //Adding a class name to style search bar
+        styleDropDownMenu={'mydropDown'} //Adding a class name to style dropdown menu
+      />
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default App;
+```
+
+```css
+// react-modal.css
+.mytextBar {
+  font-size: 1.5rem;
+  border: 1px solid blue;
+}
+
+.mysearchBar{
+  font-size: 1.5rem
+}
+
+.mydropDown li:hover{
+  background-color: red;
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,195 @@
+import React, { useEffect, useRef, useState } from 'react';
+import './CustomDropDown.css';
+
+interface objOption {
+  displayName: string,
+  value: any
+}
+
+interface Props {
+  /** 
+   * A multiple choices list
+   */
+  multipleSelect?: boolean | undefined
+  /**
+   * The select options must be [{displayName:'name', value:'value'}] format
+   */
+  options: any[]
+  /**
+   * The placeholder text for the select input 
+   */
+  textbarPlaceHolder?: string
+  /**
+   * Adding a class name to style select input 
+   */
+  textbarStyle?: string
+  /**
+   * Adding a class name to style search bar
+   */
+  searchbarStyle?: string
+  /**
+   * Adding a class name to style dropdown menu
+   */
+  styleDropDownMenu?: string
+  /** 
+   * Selected values
+   */
+  selectedValues: any
+  /** 
+   * A UseState to set selectedValues variable
+   */
+  setSelectedValues: React.Dispatch<React.SetStateAction<any>>
+}
+
+
+
+const CustomDropDown = ({
+  multipleSelect,
+  options,
+  textbarPlaceHolder,
+  textbarStyle,
+  searchbarStyle,
+  styleDropDownMenu,
+  selectedValues,
+  setSelectedValues,
+}: Props) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  const [displayName, setDisplayName] = useState<string[]>([]);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+        setSearchTerm('');
+        setFilteredOptions(options);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleCheckboxChange = (option: objOption) => {
+    if (
+      selectedValues.some(
+        (_option: { displayName: string; value: any; }) =>
+          _option.displayName === option.displayName &&
+          _option.value === option.value
+      )
+    ) {
+      setSelectedValues(
+        selectedValues.filter(
+          (item: { displayName: string; value: any; }) =>
+            item.displayName !== option.displayName &&
+            item.value !== option.value
+        )
+      );
+      setDisplayName(
+        selectedValues.filter((item: { displayName: string; value: any; }) => item !== option).displayName || []
+      );
+    } else {
+      setSelectedValues([...selectedValues, option]);
+      setDisplayName([...displayName, option.displayName]);
+    }
+  };
+
+  const handleInputChange = (event: any) => {
+    const term = event.target.value;
+    setSearchTerm(term);
+    const filtered = options.filter((option) =>
+      option.displayName?.toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredOptions(filtered);
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(false);
+  };
+
+  const handleOptionClick = (option: objOption) => {
+    setSelectedValues(option);
+    toggleDropdown();
+  };
+
+  return (
+    <>
+      {multipleSelect ? (
+        <div className="dropdown" ref={dropdownRef}>
+          <input
+            type="text"
+            placeholder={textbarPlaceHolder || 'Select...'}
+            value={[...selectedValues]
+              .map((item) => item?.displayName)
+              .join(', ')}
+            onFocus={() => setIsOpen(true)}
+            className={textbarStyle}
+            readOnly
+          />
+          {isOpen && (
+            <ul className={`${styleDropDownMenu} options`}>
+              <input
+                className={`search-bar ${searchbarStyle}`}
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleInputChange}
+              />
+              {filteredOptions.map((option, index) => (
+                <li key={index} onClick={() => handleCheckboxChange(option)}>
+                  <input
+                    type="checkbox"
+                    value={option}
+                    checked={selectedValues.some(
+                      (_option: { displayName: any; value: any; }) =>
+                        _option.displayName === option.displayName &&
+                        _option.value === option.value
+                    )}
+                    readOnly
+                  />
+                  <label htmlFor={option.displayName}>
+                    {option.displayName}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ) : (
+        <div className="dropdown" ref={dropdownRef}>
+          <input
+            type="text"
+            placeholder={textbarPlaceHolder || 'Select...'}
+            value={selectedValues}
+            onFocus={() => setIsOpen(true)}
+            className={textbarStyle}
+            readOnly
+          />
+          {isOpen && (
+            <ul className="options">
+              <input
+                className={`search-bar ${searchbarStyle}`}
+                type="text"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleInputChange}
+              />
+              {filteredOptions.map((option, index) => (
+                <li key={index} onClick={() => handleOptionClick(option)}>
+                  {option.displayName}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CustomDropDown;
